83ca0838280c47d0b44b67e4a584a9358b662656
serg <567-45@mail.ru>
2022-09-06 14:41:00 +0300

    WIP SQLite handler: almost all required functions implemented.

--- config.py
+++ config.py
@@ -3,6 +3,9 @@ discord_bot_token_filePath = "discord_token"
 urls_for_parsing = ["http://www.klin-park.ru/afisha/"] 
 # !!! какая-то проблема при парсиге https://www.klincity.ru/events/
 
+# SQLite
+lSQL_db_path = "EventsDataBase.db"
+
 # PostgreSQL 
 use_postgreSQL = True
 pSQL_adress = "127.0.0.1"
@@ -27,6 +30,7 @@ token_discord = readTokenFromFile(discord_bot_token_filePath)
 # ---------------------------------------------------------------------- 
 from entities import Event
 import datetime
+
 list_of_test_events = [Event(432, "День дверей", 
     datetime.date(2022, 3, 21), datetime.date(2022, 9, 5)), 
     Event(59, "День замков", 
@@ -35,7 +39,6 @@ list_of_test_events = [Event(432, "День дверей",
         datetime.date(2022, 12, 28), datetime.date(2022, 9, 5))]
 
 list_of_another_test_events = list_of_test_events.copy()
-
 list_of_another_test_events.append(Event(784, "День крыш", 
     datetime.date(2022, 6, 17),
     datetime.date(2022, 9, 5)))
---
+++ handler_SQLite.py
@@ -0,0 +1,99 @@
+import sqlite3
+import datetime
+
+import config
+from entities import Event
+
+db_path = config.lSQL_db_path
+
+with sqlite3.connect(db_path) as con: 
+    cursor = con.cursor()
+    cursor.execute("""CREATE TABLE IF NOT EXISTS events(
+                    id INTEGER PRIMARY KEY AUTOINCREMENT, 
+                    title VARCHAR(100) DEFAULT 'NO_TITLE', 
+                    event_date DATE, 
+                    date_added DATE DEFAULT CURRENT_TIMESTAMP);""")
+
+
+def convert_str_to_date(string: str):
+    """
+    This func converts date-string from DB to datetime.data, because 
+    SQLite3 does not do it automaticaly.
+    Returns datetime.date() object
+    """
+    
+    date_list = list(map(int, string[0:10].split("-")))
+    return datetime.date(date_list[0], date_list[1], date_list[2])
+
+
+def execute_query(query: str):
+    """
+    Connects to DB and returns cursor-object.
+    """
+    with sqlite3.connect(db_path) as con: 
+        cursor = con.cursor()
+        cursor.execute(query)
+        return cursor
+
+
+def get_list_of_events_all():
+    "Returns list of Events"
+
+    events_list = []
+    query = "SELECT * FROM events"
+    data_list = execute_query(query).fetchall()
+
+    for record in data_list:
+        event_date = convert_str_to_date(record[2])
+        date_added = convert_str_to_date(record[3])
+        events_list.append(Event(int(record[0]), record[1], 
+            event_date, date_added))
+
+    return events_list
+
+
+def put_list_of_events(list_of_events: list):
+    "Checks is the same events from list already in DB. If not - puts them in DB"
+
+    list_of_events_from_db = get_list_of_events_all()
+
+    # Check for duplicates in DB
+    for db_event in list_of_events_from_db:
+        if len(list_of_events) == 0:
+            break
+
+        for event in list_of_events:
+            if (event.title == db_event.title 
+                    and event.event_date == db_event.event_date):
+                list_of_events.remove(event)
+                break
+
+    # Put remaining events in DB
+    for event in list_of_events:
+        execute_query("INSERT INTO events(title, event_date) " + 
+                "VALUES('" + event.title + "', '" + str(event.event_date) + 
+                "');")
+
+
+def get_events_sheduled_for(number_of_days: int):
+    "Returns events from DB, which will happen in next number_of_days"
+
+    today_date = datetime.date.today()
+    number_of_days = datetime.timedelta(days=number_of_days)
+    zero_day = datetime.timedelta(days=0)
+
+    list_of_events_from_db = get_list_of_events_all()
+    filtered_list = []
+
+    for event in list_of_events_from_db:
+        if (zero_day <= (event.event_date - today_date) <= number_of_days):
+            filtered_list.append(event)
+
+    return filtered_list
+
+
+def clear_overdues():
+    list_of_events_from_db = get_list_of_events_all()
+    # !!! WIP
+
+
