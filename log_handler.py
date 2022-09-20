1f4272e43a4cb86502025dfaf01f49b49cccba0c
serg <567-45@mail.ru>
2022-09-20 14:30:19 +0300

    Logs: added logger-module; added logs to main-modele

---
+++ log_handler.py
@@ -0,0 +1,14 @@
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+# настройка обработчика и форматировщика для logger2
+handler = logging.FileHandler("logfile", mode='w')
+formatter = logging.Formatter("%(name)s %(asctime)s %(levelname)s %(message)s")
+
+# добавление форматировщика к обработчику
+handler.setFormatter(formatter)
+# добавление обработчика к логгеру
+logger.addHandler(handler)
+
--- main.py
+++ main.py
@@ -2,25 +2,38 @@ import threading
 import time
 
 
-from config import (token_discord, refresh_time)
+from config import token_discord, refresh_time
+from log_handler import logger
 import bot_discord
 import parser_master 
 import db_handler 
 
 
-def start_bots():
-    ParserMaster = parser_master.ParserMaster()
+logger.debug("MAIN: run.")
 
+def start_bots():
+    """
+    Infinetly restartable function for refreshing DB and bots. By restrting, 
+    bots notify users about new evevnts.
+    """
+
+    logger.info("MAIN: starting DB refresh and bot-threads...")
+    ParserMaster = parser_master.ParserMaster()
     db_handler.put_list_of_events(
             ParserMaster.parse_urls_from_config()) # update DB
+    logger.info("MAIN: DB has been refreshed successfuly.")
 
     thread_discord = threading.Thread(target=bot_discord.bot.run(token_discord),
             name='thrd-DiscordBot', daemon=True)
     thread_discord.start()
+    logger.info("MAIN: discord bot thread has been run.")
 
     time.sleep(refresh_time)
+    logger.info("MAIN: refresh time is out - initiate restarting process...")
 
+    logger.info("MAIN: closing bot-threads...")
     thread_discord.cancel() # TODO: need to fix that
+    logger.info("MAIN: bot-threads closed successfuly.")
 
 
 while True:
